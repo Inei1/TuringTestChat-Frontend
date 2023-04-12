d807f104b4f8a7de14c1649cd51522ea91e4ed41
Inei1 <neilrmoon@gmail.com>
2023-04-11 20:45:48 -0700

    Start work on chatroom ui following tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Chat } from './a/a';
+import { io } from 'socket.io-client';
+
+const socket = io("http://localhost:4000");
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [room, setRoom] = useState('');
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element:
+        <Chat
+        username={username}
+        setUsername={setUsername}
+        room={room}
+        setRoom={setRoom}
+        socket={socket} />
+    }
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
 
 export default App;
+
