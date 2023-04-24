fc2da23ba0df6130f0b78dcde8208693c1dbb66f
Inei1 <neilrmoon@gmail.com>
2023-04-23 17:18:29 -0700

    add sitemap and link noreferrer security

--- src/homepage/Footer.tsx
+++ src/homepage/Footer.tsx
@@ -9,16 +9,16 @@ export const Footer = () => {
     <Container>
       <Grid sx={{ mt: 10 }} container spacing={1}>
         <Grid item xs={5} md={0}>
-          <Link target="_blank" href="mailto:support@turingtestchat.com" color="#e9e9e9" fontFamily="monospace" fontSize={18}>Contact</Link>
+          <Link target="_blank" rel="noreferrer" href="mailto:support@turingtestchat.com" color="#e9e9e9" fontFamily="monospace" fontSize={18}>Contact</Link>
         </Grid>
         <Grid item xs={5} md={0}>
-          <Link target="_blank" href="https://old.reddit.com/r/TuringTestChat/" color="#e9e9e9" fontFamily="monospace" fontSize={18}>Reddit</Link>
+          <Link target="_blank" rel="noreferrer" href="https://old.reddit.com/r/TuringTestChat/" color="#e9e9e9" fontFamily="monospace" fontSize={18}>Reddit</Link>
         </Grid>
         <Grid item xs={5} md={0}>
           <Link onClick={() => navigate("/tos")} href="/tos" color="#e9e9e9" fontFamily="monospace" fontSize={18}>Terms of Service</Link>
         </Grid>
         <Grid item xs={5} md={0}>
-          <Link target="_blank" href="https://discord.com/invite/SX48DMUb5H" color="#e9e9e9" fontFamily="monospace" fontSize={18}>Discord</Link>
+          <Link target="_blank" rel="noreferrer" href="https://discord.com/invite/SX48DMUb5H" color="#e9e9e9" fontFamily="monospace" fontSize={18}>Discord</Link>
         </Grid>
         <Grid item xs={5} md={0}>
           <Link onClick={() => navigate("/privacypolicy")} href="/privacypolicy" color="#e9e9e9" fontFamily="monospace" fontSize={18}>Privacy Policy</Link>
