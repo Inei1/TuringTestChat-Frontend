d15a0498041fbf717a33fec69daee525ce15264b
Inei1 <neilrmoon@gmail.com>
2023-04-23 17:40:12 -0700

    update headers

--- src/homepage/Features.tsx
+++ src/homepage/Features.tsx
@@ -11,24 +11,24 @@ export const Features = () => {
         <Grid container spacing={5}>
           <Grid item xs={12} md={4}>
             <Box sx={{}}>
-              <Typography variant="h5" sx={{ my: 2 }}>Perform the turing test</Typography>
-              <Typography variant="h6">When you enter the chatroom on the website, you will be paired with a human or with ChatGPT.
+              <Typography variant="h2" sx={{ my: 2, fontSize: 25 }}>Perform the turing test</Typography>
+              <Typography sx={{ fontSize: 18}}>When you enter the chatroom on the website, you will be paired with a human or with ChatGPT.
               Your task is to perform the Turing Test by identifying which of the two you think you were talking to. Do you have
               what it takes to accurately distinguish between human and AI?</Typography>
             </Box>
           </Grid>
           <Grid item xs={12} md={4}>
             <Box sx={{}}>
-              <Typography variant="h5" sx={{ my: 2 }}>A game of deception</Typography>
-              <Typography variant="h6">Put your deception and detective skills to the test by outsmarting the other person.
+              <Typography variant="h2" sx={{ my: 2, fontSize: 25 }}>A game of deception</Typography>
+              <Typography sx={{ fontSize: 18}}>Put your deception and detective skills to the test by outsmarting the other person.
               You will be randomly assigned one of two tasks - to convince them that you are a bot or a human.
               But don't forget, they are trying to do the same!</Typography>
             </Box>
           </Grid>
           <Grid item xs={12} md={4}>
             <Box sx={{}}>
-              <Typography variant="h5" sx={{ my: 2 }}>Cutting edge AI chat</Typography>
-              <Typography variant="h6">With ChatGPT's latest AI, distinguishing between a chatbot and
+              <Typography variant="h2" sx={{ my: 2, fontSize: 25 }}>Cutting edge AI chat</Typography>
+              <Typography sx={{ fontSize: 18}}>With ChatGPT's latest AI, distinguishing between a chatbot and
                 a human won't be easy. The AI is optimized to accurately emulate human conversation and will
                 do anything to trick you. Are you up for the challenge of distinguishing between artificial
                 and human intelligence?</Typography>
--- src/homepage/Landing.tsx
+++ src/homepage/Landing.tsx
@@ -12,8 +12,8 @@ export const Landing = () => {
           maxWidth: "50vw",
           mt: 5
         }}>
-        <Box component="img" alt="TuringTestChat logo" src="TTCLogov2.png" sx={{ maxWidth: "25vh",}}></Box>
-        <h1 style={{ fontFamily: "monospace", fontSize: 100, fontWeight: "normal", color: "#e9e9e9" }}>Turing Test Chat</h1>
+        <Box component="img" alt="TuringTestChat logo" src="TTCLogov2.png" sx={{ maxWidth: "25vh", }}></Box>
+        <Typography variant="h1" style={{ fontFamily: "monospace", fontSize: 100, fontWeight: "normal", color: "#e9e9e9" }}>Turing Test Chat</Typography>
         <Grid sx={{ mt: 1, ml: "40%" }} container spacing={1}>
           <Grid item xs={12} md={5}>
             <Typography>"The Turing test... is a test of a machine's ability to exhibit intelligent behaviour equivalent to, or indistinguishable from, that of a human."</Typography>
