dc39c5eeec2253c98894303e03b77a38dd3d4628
Inei1 <neilrmoon@gmail.com>
2023-04-25 18:28:25 -0700

    Add Facebook and Twitter to footer

--- src/homepage/Footer.tsx
+++ src/homepage/Footer.tsx
@@ -7,19 +7,25 @@ export const Footer = () => {
     <Container>
       <Grid sx={{ mt: 10 }} container spacing={1}>
         <Grid item xs={5} md={0}>
+          <MuiLink target="_blank" rel="noreferrer" href="https://old.reddit.com/r/TuringTestChat/" color="#e9e9e9" fontFamily="monospace" fontSize={18}>Reddit</MuiLink>
+        </Grid>
+        <Grid item xs={5} md={0}>
           <MuiLink target="_blank" rel="noreferrer" href="mailto:support@turingtestchat.com" color="#e9e9e9" fontFamily="monospace" fontSize={18}>Contact</MuiLink>
         </Grid>
         <Grid item xs={5} md={0}>
-          <MuiLink target="_blank" rel="noreferrer" href="https://old.reddit.com/r/TuringTestChat/" color="#e9e9e9" fontFamily="monospace" fontSize={18}>Reddit</MuiLink>
+        <MuiLink target="_blank" rel="noreferrer" href="https://twitter.com/TuringTestChat" color="#e9e9e9" fontFamily="monospace" fontSize={18}>Twitter</MuiLink>
         </Grid>
         <Grid item xs={5} md={0}>
           <Link to="/tos" style={{ color: "#e9e9e9", fontFamily: "monospace", fontSize: 18 }}>Terms of Service</Link>
         </Grid>
         <Grid item xs={5} md={0}>
-          <MuiLink target="_blank" rel="noreferrer" href="https://discord.com/invite/SX48DMUb5H" color="#e9e9e9" fontFamily="monospace" fontSize={18}>Discord</MuiLink>
+          <MuiLink target="_blank" rel="noreferrer" href="https://www.facebook.com/people/Turingtestchat/100092302895572/" color="#e9e9e9" fontFamily="monospace" fontSize={18}>Facebook</MuiLink>
         </Grid>
         <Grid item xs={5} md={0}>
           <Link to="/privacypolicy" style={{ color: "#e9e9e9", fontFamily: "monospace", fontSize: 18 }}>Privacy Policy</Link>
+        </Grid>
+        <Grid item xs={5} md={0}>
+          <MuiLink target="_blank" rel="noreferrer" href="https://discord.com/invite/SX48DMUb5H" color="#e9e9e9" fontFamily="monospace" fontSize={18}>Discord</MuiLink>
         </Grid>
       </Grid>
       <Box sx={{ minHeight: 100 }}></Box>
