2f7844ca88fb2b4e96043c387f02bcbff1afb116
Inei1 <neilrmoon@gmail.com>
2023-05-27 22:59:45 -0700

    Add some elements to beta

--- src/homepage/Landing.tsx
+++ src/homepage/Landing.tsx
@@ -25,7 +25,16 @@ export const Landing = () => {
         }}>
         <Box component="img" alt="Turing Test Chat logo" src="TTCLogov2.png" sx={{ maxWidth: "25vh", }} />
         <Typography variant="h1" style={{ fontFamily: "monospace", fontSize: 100, fontWeight: "normal", color: "#e9e9e9" }}>Turing Test Chat</Typography>
-        
+        <Grid sx={{ mt: 1, ml: "40%", my: 3 }} container spacing={1}>
+          <Grid container spacing={5}>
+            <Grid item>
+              <Typography variant="h3">The beta is live!</Typography>
+            </Grid>
+            <Grid item>
+              <Button variant="contained" size="large" onClick={() => navigate("/home")}>Try it out</Button>
+            </Grid>
+          </Grid>
+        </Grid>
         <Button variant="contained" onClick={() => navigate("/betafaq")}>beta FAQ</Button>
       </Container>
     </>
