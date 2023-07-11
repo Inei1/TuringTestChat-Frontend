24f2a6a5b75ddfabb0449a63a8cb6f9834bf7b9b
Inei1 <neilrmoon@gmail.com>
2023-07-11 14:40:50 -0700

    Fix some possible policy violations for google ads

--- src/pages/privacypolicy/index.tsx
+++ src/pages/privacypolicy/index.tsx
@@ -36,9 +36,16 @@ const PrivacyPolicy = () => {
             Your personal data will not be sold, rented, or otherwise disclosed to any third party without your explicit consent.</Typography>
           <Typography sx={{ my: 1 }}>The personal data collected from you may include your email address.
             Turing Test Chat may also collect non-personally identifiable information such as your IP address and browsing behavior.
-            Turing Test Chat will store your chat messages.</Typography>
+            Turing Test Chat will store your chat messages, and may share your anonymous chats with third-parties with all personal information, such as your email and username, removed.
+            Turing Test Chat may use cookies or local storage for authentication and other site functionality purposes.
+            This information will not be sold or disclosed to any third party for any reason.
+            By using Turing Test Chat, you agree to have this information stored in your browser.
+            If you wish to withdraw consent, you may do so without charge at any time by contacting <Link target="_blank" href="mailto:support@turingtestchat.com">Turing Test Chat support</Link>.</Typography>
           <Typography sx={{ my: 1 }}>Third party vendors, including Google, use cookies to serve ads based on a user's prior visits to your website or other websites.
             Google's use of advertising cookies enables it and its partners to serve ads to your users based on their visit to your sites and/or other sites on the Internet.
+            Third parties may place and read cookies on your browser or use web beacons to collect information for advertising purposes.
+            These cookies may be used to serve personalized ads.
+            To learn more about how Google uses your data, you can visit <Link target="_blank" href="http://www.google.com/policies/privacy/partners/">http://www.google.com/policies/privacy/partners/</Link>.
             Users may opt out of personalized advertising by visiting Google's <Link target="_blank" href="https://www.google.com/settings/ads">Ads Settings</Link>.
             Additionally, the third-party ad service <Link target="_blank" href="https://www.ezoic.com/">Ezoic</Link> may serve peronalized ads.
             You may view Ezoic's part of the Turing Test Chat <Link target="_blank" href="https://g.ezoic.net/privacy/turingtestchat.com">privacy policy here</Link>.
